f3c657249f08add2089ef1b1de47da7109fd7c60
frescobar <frescobar87@gmail.com>
2020-06-19 18:30:49 -0500

    Fetch the results from an API (unsplash) from users inputs

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-function App() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+export default class App extends Component {
+  state = { images: [] };
+  onFormSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID yOS72NAd6_K3xHOvUZ9SwLgMd1ene3RebCigOjrpphc",
+      },
+    });
+    this.setState({
+      images: response.data.results,
+    });
+  };
+  render() {
+    const { images } = this.state;
+    return (
+      <div>
+        <SearchBar onFormSubmit={this.onFormSubmit} />
+        <p style={{ textAlign: "center" }}>Results: {images.length} images</p>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-
 export default class SearchBar extends Component {
   state = {
     term: "",
   };
+  onSubmitHandler = (e) => {
+    e.preventDefault();
 
+    this.props.onFormSubmit(this.state.term);
+    this.setState({
+      term: "",
+    });
+  };
   render() {
     return (
       <div className="ui grid centered padded">
         <div className="six wide computer twelve wide mobile column ">
           <div className=" ui segment">
-            <form className=" ui form">
+            <form className=" ui form" onSubmit={this.onSubmitHandler}>
               <div className="field">
                 <label>Search Image</label>
                 <input
